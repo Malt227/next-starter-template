453e452e5e34f11b876fb925d3347aae09af0824
Malt227 <65109623+Malt227@users.noreply.github.com>
2025-06-04 15:31:20 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,8 @@ export default function Home() {
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/fencefortw.png"
           alt="FenceFort Logo"
-          width={180}
-          height={37}
+          width={500}
+          height={500}
           priority
         />
       </div>
